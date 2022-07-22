cf836afd7fd6d98e86f02b26d09969922b31f443
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2022-07-22 20:00:16 +0300

    search, weather components

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		'X-RapidAPI-Key': 'fad9117dbamsh2a74d95374ea827p13e081jsn1c723badaeaa',
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "7ff3950717d7293f10c8b0a728b822e6";
